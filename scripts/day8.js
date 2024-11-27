b8e4edb4c570f04f341ece13d199b9031b2d9e7f
Isaac ILIWA <isaaciliwa5@gmail.com>
2024-11-27 19:29:23 +0300

    a lot of files added

---
+++ scripts/day8.js
@@ -0,0 +1,12 @@
+function greetToday () {
+    console.log("Hello World!");
+}
+
+greetToday();
+
+let name = Alice;
+function greetUser(name) {
+    console.log("Hello and Welcome ${name}!");
+} 
+
+greetUser();
